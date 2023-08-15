e28620c3c646491455c896bfa5410b647c335c89
drimerdev <miguelvicente18@aol.com>
2023-08-15 18:42:01 -0300

    move to electron
    aaaaaaaaaaaaaaaaaaaaaa

---
+++ app/web-inf.js
@@ -0,0 +1,7 @@
+        box = document.getElementById("box")
+        iframe = document.getElementById("browser")
+
+      function updateIframe(){
+          iframe.src = box.value
+      }
+// delet the "/" after use the script.
